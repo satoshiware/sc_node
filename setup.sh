957613d9da347156d744a1bf21bbac43217039af
satoshiware <satoshiware@gmail.com>
2026-01-23 17:26:20 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#################
+# Executed from firstboot.sh script where stdout is redirected to firstboot's logging (var/log/firstboot.log)
+################
 set -euo pipefail
+
+echo "Updating and upgrading..."; sleep 2
+sudo apt update && sudo apt upgrade -y
 
 echo "setup.sh completed at $(date)" >> /var/log/setup.log
